7feeb2321ac202453e8944971d3e8e0a4498ffe5
hostar <aaa@bbb.com>
2020-07-13 22:39:11 +0200

    Scene2Parser project added

---
+++ Scene2Parser/DataLayer/DncCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MafiaSceneEditor.DataLayer
+{
+    public class DncCollection
+    {
+        public ObservableCollection<DncCollection> Items { get; set; } = new ObservableCollection<DncCollection>();
+        public NodeTag Tag { get; set; }
+        public string Name { get; set; }
+
+        public DncCollection(string name)
+        {
+            Name = name;
+        }
+    }
+}
