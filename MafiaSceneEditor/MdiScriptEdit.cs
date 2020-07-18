b57dc6844a0a99b2a373c7229f106e8eb491416e
hostar <janstarka@gmail.com>
2020-07-18 19:20:28 +0200

    scintilla added, conflicting menu items deleted

--- MafiaSceneEditor/MdiScriptEdit.cs
+++ MafiaSceneEditor/MdiScriptEdit.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Media;
-using FastColoredTextBoxNS;
+//using FastColoredTextBoxNS;
+using ScintillaNET;
 
 namespace MafiaSceneEditor
 {
     public partial class MdiScriptEdit : Form
     {
-        private FastColoredTextBox textEditor;
+        //private FastColoredTextBox textEditor;
+        private Scintilla scintillaTextEditor;
         public MdiScriptEdit()
         {
             InitializeComponent();
@@ -22,13 +25,30 @@ namespace MafiaSceneEditor
             toolStripUndo.Image = Resources.undo;
             toolStripUndo.Click += ToolStripUndo_Click;
 
+            /*
             textEditor = new FastColoredTextBoxNS.FastColoredTextBox
             {
                 Parent = mainPanel,
                 Dock = DockStyle.Fill
             };
+            */
 
-            mainPanel.Controls.Add(textEditor);
+            scintillaTextEditor = new Scintilla
+            {
+                //WrapMode = WrapMode.Word, 
+                //IndentationGuides = IndentView.LookBoth, 
+                //Parent = mainPanel, 
+                Dock = DockStyle.Fill
+            };
+
+            scintillaTextEditor.Styles[ScintillaNET.Style.Default].Font = "Consolas";
+            scintillaTextEditor.Styles[ScintillaNET.Style.Default].Size = 10;
+
+            //scintillaTextEditor.AssignCmdKey(Keys.Control | Keys.C , Command.)
+            //scintillaTextEditor.ExecuteCmd(ScintillaNET.Command.)
+
+            //mainPanel.Controls.Add(textEditor);
+            mainPanel.Controls.Add(scintillaTextEditor);
 
             tableLayoutPanel1.SetColumnSpan(mainPanel, 3);
             tableLayoutPanel1.SetRowSpan(mainPanel, 1);
@@ -45,12 +65,14 @@ namespace MafiaSceneEditor
 
         private void ToolStripUndo_Click(object sender, EventArgs e)
         {
-            textEditor.Undo();
+            //textEditor.Undo();
+            scintillaTextEditor.Undo();
         }
 
         public void SetEditorText(string text)
         {
-            textEditor.Text = text;
+            //textEditor.Text = text;
+            scintillaTextEditor.Text = text;
         }
 
         private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
