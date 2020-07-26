0faa64ef3b26bef6b40d2d20a8bb10f2ff7200d4
hostar <janstarka@gmail.com>
2020-07-26 21:39:30 +0200

    Solution rename

--- MafiaSceneEditor/Resources.Designer.cs
+++ MafiaSceneEditor/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MafiaSceneEditor {
+namespace YAMSE {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MafiaSceneEditor {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MafiaSceneEditor.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("YAMSE.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
