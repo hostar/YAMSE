95b572780490fc84852da12958afa38051e51db3
hostar <janstarka@gmail.com>
2020-08-02 23:29:30 +0200

    Krypton layout works except ribbon

--- MafiaSceneEditor/MainForm2.cs
+++ MafiaSceneEditor/MainForm2.cs
@@ -1,12 +1,8 @@
 ﻿using ComponentFactory.Krypton.Navigator;
 using ComponentFactory.Krypton.Toolkit;
 using ComponentFactory.Krypton.Workspace;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 namespace YAMSE
@@ -22,8 +18,11 @@ namespace YAMSE
         ToolStripMenuItem newToolStripMenuItem = new ToolStripMenuItem();
         ToolStripMenuItem fileToolStripMenuItem = new ToolStripMenuItem();
 
+        KryptonTreeView treeView1 = new KryptonTreeView();
+
         public MainForm2()
         {
+            // KryptonExplorer
             InitializeComponent();
             SuspendLayout();
             ((ISupportInitialize)kryptonWorkspaceTreeView).BeginInit();
@@ -31,14 +30,15 @@ namespace YAMSE
             kryptonWorkspaceTreeView.Dock = DockStyle.Fill;
             ((ISupportInitialize)kryptonWorkspaceTreeView).EndInit();
 
-            kryptonWorkspaceTreeView.Root.Children.Add(CreateCell(1, false, false));
+            kryptonWorkspaceTreeView.Root.Children.Add(CreateCell("Objects", NavigatorMode.Panel));
 
             kryptonWorkspaceTreeView.AllowPageDrag = false;
 
             ((ISupportInitialize)kryptonWorkspaceContent).BeginInit();
             kryptonWorkspaceContent.Dock = DockStyle.Fill;
             ((ISupportInitialize)kryptonWorkspaceContent).EndInit();
-            kryptonWorkspaceContent.Root.Children.Add(CreateCell(2));
+
+            //kryptonWorkspaceContent.Root.Children.Add(CreateCell(2));
 
             Controls.Add(splitContainer);
             Controls.Add(menuStrip1);
@@ -54,24 +54,24 @@ namespace YAMSE
 
         private void InitMenus()
         {
-            this.newToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
-            this.newToolStripMenuItem.Text = "&New";
+            newToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.ShortcutKeys = (Keys.Control | Keys.N);
+            newToolStripMenuItem.Size = new Size(181, 26);
+            newToolStripMenuItem.Text = "&New";
 
             fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.newToolStripMenuItem });
+            newToolStripMenuItem });
 
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
-            this.fileToolStripMenuItem.Text = "&File";
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            fileToolStripMenuItem.Text = "&File";
 
             menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem });
+            fileToolStripMenuItem });
         }
 
-        private KryptonWorkspaceCell CreateCell(int numPages, bool allowDrag = true, bool allowDrop = true)
+        private KryptonWorkspaceCell CreateCell(string pageName, NavigatorMode mode = NavigatorMode.BarTabGroup)
         {
             // Create new cell instance
             KryptonWorkspaceCell cell = new KryptonWorkspaceCell();
@@ -81,52 +81,29 @@ namespace YAMSE
             if (!string.IsNullOrEmpty(starSize))
                 cell.StarSize = starSize;
             */
-
-            // Add requested number of pages
-            for (int i = 0; i < numPages; i++)
-            {
-                cell.Pages.Add(CreatePage());
-            }
-
-            cell.AllowPageDrag = allowDrag;
-            cell.PageDrop += Cell_PageDrop;
+            cell.NavigatorMode = mode;
+            cell.Pages.Add(CreatePage(pageName));
 
             return cell;
         }
 
-        private void Cell_PageDrop(object sender, PageDropEventArgs e)
+        private KryptonPage CreatePage(string pageName)
         {
-            e.Cancel = true;
-        }
-
-        private KryptonPage CreatePage()
-        {
-            // Give each page a unique number
-            string pageNumber = 1.ToString();
-
             // Create a new page and give it a name and image
             KryptonPage page = new KryptonPage();
-            page.Text = "P" + pageNumber;
-            page.TextTitle = "P" + pageNumber + " Title";
-            page.TextDescription = "P" + pageNumber + " Description";
+            page.Text = pageName;
+            page.TextTitle = pageName;
+            page.TextDescription = pageName;
             //page.ImageSmall = imageList.Images[_count % imageList.Images.Count];
             page.MinimumSize = new Size(200, 250);
 
-            // Create a rich text box with some sample text inside
-            KryptonRichTextBox rtb = new KryptonRichTextBox();
-            rtb.Text = "This page (" + page.Text + ") contains a rich text box control as example content. Your application could place anything you like here such as data entry controls, charts, data grids etc.\n\nTry dragging the page headers in order to rearrange the workspace layout.";
-            rtb.Dock = DockStyle.Fill;
-            rtb.StateCommon.Border.Draw = InheritBool.False;
+            treeView1.Dock = DockStyle.Fill;
 
             // Add rich text box as the contents of the page
             page.Padding = new Padding(5);
-            page.Controls.Add(rtb);
+            page.Controls.Add(treeView1);
 
             return page;
-        }
-
-        private void Page_DragDrop(object sender, DragEventArgs e)
-        {
         }
     }
 }
