4fdcb5f34a7d31135ec3953ac23e983402c261df
hostar <janstarka@gmail.com>
2022-01-23 00:44:20 +0100

    minor cleanup

--- MafiaSceneEditor/Program.cs
+++ MafiaSceneEditor/Program.cs
@@ -17,7 +17,6 @@ namespace YAMSE
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new MainForm());
             Application.Run(new MainForm2());
         }
     }
