aa981349b18c31575054cdb1efd1835df177c386
hostar <janstarka@gmail.com>
2022-02-09 23:23:32 +0100

    Support for different line ends

---
+++ MafiaSceneEditor/Helpers/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YAMSE.Helpers
+{
+    public static class Extensions
+    {
+        public static string[] SplitByCorrect(this string str)
+        {
+            int howManyR = str.Count(x => x == '\r');
+            int howManyN = str.Count(x => x == '\n');
+
+            if (howManyR < (howManyN - 10))
+            {
+                return str.Split("\n");
+            }
+            else
+            {
+                return str.Split("\r\n");
+            }
+        }
+    }
+}
